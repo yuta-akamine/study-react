9b0602ed75f6f4f38519cda29f3c58ed0b396e31
yuta-akamine <happykaizokuou@gmail.com>
2023-07-03 15:38:43 +0900

    ⚡️ useEffectを導入

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,9 @@ import { Main } from '@/components/Main'
 // import { Main } from '@/src/components/Main'
 import { Header } from '@/components/Header'
 import Link from 'next/link'
-import { useCallback } from 'react'
+
+// ReactからuseCallback、useEffectをインポートしている
+import { useCallback, useEffect } from 'react'
 // import { Header } from '@/src/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,6 +20,18 @@ export default function Home() {
     console.log(e.target.href); // aタグをクリックするとDOM aのhref属性の値(url)がコンソールに出力される
     e.preventDefault(); //画面遷移を無効にしている。
     alert(foo);
+  }, []);
+
+  // useEffectを利用してアロー関数を定義
+  useEffect(() => { // このファイルHomeコンポーネントがマウント(レンダリングされる瞬間)時の処理。
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue"; //背景色をlightblueにする
+
+    //useEffectにreturnを記述することでアンマウント時の処理を記述できる。
+    return () => { // アンマウント時の処理。Cleanup Functionともいう
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = ""; //デフォルトの背景色にする
+    };
   }, []);
   
   return (
