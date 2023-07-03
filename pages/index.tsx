e25d58c484354f50af44149bcfff2f30ee8f9fe7
yuta-akamine <happykaizokuou@gmail.com>
2023-07-03 10:03:14 +0900

    ⚗️ 不要な記述でいつか削除、クリックイベントを記述

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,35 @@ import { Inter } from 'next/font/google'
 import { Main } from '@/components/Main'
 // import { Main } from '@/src/components/Main'
 import { Header } from '@/components/Header'
+import Link from 'next/link'
+import { useCallback } from 'react'
 // import { Header } from '@/src/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const foo = 1;
+
+  // 無名関数は使えないので関数名をつける
+  const handleClick = useCallback((e) => { // 第一引数eにはクリックした際のイベントが入っている
+    // イベントのtargetプロパティはDOMになったaタグを値に持つ
+    console.log(e.target.href); // aタグをクリックするとDOM aのhref属性の値(url)がコンソールに出力される
+    e.preventDefault(); //画面遷移を無効にしている。
+    alert(foo);
+  }, []);
+  
   return (
     <div>
       <Header />
+      
+      <Link
+        href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </Link>
+
       {/* propsでpageを渡している */}
       <Main page="index" />
     </div>
