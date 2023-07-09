6c06be069f1b5029b8ed0961c0fee70102fa8f98
yuta-akamine <happykaizokuou@gmail.com>
2023-07-09 17:00:27 +0900

    🎨 Paperコンポーネント利用

---
+++ components/TourCard.js
@@ -0,0 +1,8 @@
+import Paper from '@mui/material/Paper';
+
+const TourCard = () => {
+  // return <div>Hello</div>;
+  return <Paper elevation={3}>Hello</Paper>;
+};
+
+export default TourCard;
---
+++ pages/mui.tsx
@@ -0,0 +1,10 @@
+import classes from '@/pages/index.module.css'
+import TourCard from '@/components/TourCard'
+
+export default function Home() {
+  return (
+    <div>
+      <TourCard />
+    </div>
+  )
+}
